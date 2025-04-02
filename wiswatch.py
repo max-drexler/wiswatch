63cdacc5fadb8f8a4c7e82904063273645f3d935
Max Drexler <mndrexler@wisc.edu>
2025-04-02 11:08:56 -0500

    add format printing

--- wiswatch.py
+++ wiswatch.py
@@ -18,13 +18,13 @@ import functools
 import json
 import logging
 import sys
+from dataclasses import dataclass, field
 from ssl import create_default_context
-from dataclasses import dataclass, field, fields
 from urllib.parse import urlparse
 
 import aiomqtt
 
-LOG = logging.getLogger('wiswatch')
+LOG = logging.getLogger("wiswatch")
 
 
 def port_per_transport(transport: str) -> int:
@@ -32,7 +32,8 @@ def port_per_transport(transport: str) -> int:
         return 8883
     if transport.lower() in ("websockets", "websocket", "ws", "wss"):
         return 443
-    raise ValueError(f"Unknown transport: {transport}")
+    msg = f"Unknown transport: {transport}"
+    raise ValueError(msg)
 
 
 def default_topics():
@@ -95,9 +96,12 @@ class WISConsumer:
             LOG.critical(err_msg)
             raise ValueError(err_msg)
 
-        conn_kwargs = {"transport": transport}
+        conn_kwargs: dict = {"transport": transport}
         if host is not None:
             conn_kwargs["hostname"] = host
+
+        if port is not None:
+            conn_kwargs["port"] = port
 
         if o.username is not None:
             conn_kwargs["username"] = o.username
@@ -162,41 +166,78 @@ async def emit_json(msg, ident=None, end="\n"):
     sys.stdout.flush()
 
 
+def format_emit(fmt_str: str):
+    async def emitter(msg):
+        sys.stdout.write(fmt_str.format_map(msg) + '\n')
+        sys.stdout.flush()
+    return emitter
+
+
 # TODO: have the option to download data using URL in payload.
 # async def download(msg):
 #     pass
 
+WISWATCH_EPILOG = """For more information on WIS2, see the following:
+    - Topic hierarchy: https://github.com/wmo-im/wis2-topic-hierarchy/tree/main
+    - Payload format: https://wmo-im.github.io/wis2-notification-message/standard/wis2-notification-message-STABLE.html
+"""
+
 
 def parse_cli_args():
-    parser = argparse.ArgumentParser(prog="wiswatch", allow_abbrev=False)
+    parser = argparse.ArgumentParser(
+        prog="wiswatch",
+        allow_abbrev=False,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=WISWATCH_EPILOG,
+    )
 
     parser.add_argument("-V", "--version", action="store_true", help="Show version information and exit.")
-    parser.add_argument("-v", "--verbose", action="count", default=None, help="Increase the verbosity of log output.")
-    parser.add_argument("-q", "--quiet", action="store_true", help="Disable all log output to stderr.")
-    parser.add_argument(
-        "-0", "--null", action="store_true", help="Use NULL ('\\0') characters to separate output messages."
-    )
+    # parser.add_argument(
+    #     "-0", "--null", action="store_true", help="Use NULL ('\\0') characters to separate output messages."
+    # )
     parser.add_argument(
         "-T",
         "--topic",
         action="store_true",
         help="Include the topic of the message in the payload as the key `__topic__`.",
     )
+    parser.add_argument("--explain", action="store_true", help="Show parsed action/connection info and exit.")
     parser.add_argument(
         "uris",
         nargs="*",
         help="Connection and/or subscription information: [{'mqtts'|'wss'}://][user]:[password]@[host]:[port][/topic]",
     )
 
-    action_group = parser.add_argument_group("Actions")
+    log_parser = parser.add_argument_group("Log Options")
+    verbosity_group = log_parser.add_mutually_exclusive_group(required=False)
+    verbosity_group.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbosity",
+        metavar="LEVEL",
+        choices=["DEBUG", "INFO", "WARNING", "CRITICAL", "ERROR"],
+        default="WARNING",
+        help="Verbosity of %(prog)s (ERROR, CRITICAL, WARNING, INFO, DEBUG). Default %(default)s.",
+    )
+    # If -q is specified args.verbosity will be None
+    verbosity_group.add_argument(
+        "-q", "--quiet", dest="verbosity", action="store_const", const=None, help="Disable all log output to stderr."
+    )
+
+    # Only allowed to choose one action
+    # args.action must be a coroutine that accepts one argument, the message.
+    action_group = parser.add_argument_group(
+        "Actions", "Choose one action to perform on all messages. Default is --print"
+    )
     action_parser = action_group.add_mutually_exclusive_group(required=False)
+    # Follow these kwargs for adding an action that doesn't accept args
     action_parser.add_argument(
         "-p",
         "--print",
         dest="action",
         action="store_const",
         const=emit_json,
-        help="The default action. Print all message payloads.",
+        help="Print JSON-serialized message payloads.",
     )
     action_parser.add_argument(
         "-P",
@@ -204,7 +245,16 @@ def parse_cli_args():
         dest="action",
         action="store_const",
         const=functools.partial(emit_json, ident=2),
-        help="Pretty print all message payloads.",
+        help="Pretty print message payloads.",
+    )
+    # Follow these kwargs for adding an action that accepts arguments
+    action_parser.add_argument(
+        "-F",
+        "--fprint",
+        metavar="FSTRING",
+        dest="action",
+        type=format_emit,
+        help="Print a string based on keys in the payload. E.g. '{properties.data_id}:{properties.start_time}'",
     )
 
     args = parser.parse_args()
@@ -213,9 +263,6 @@ def parse_cli_args():
         sys.stdout.write(f"{parser.prog}: {__version__}\n")
         sys.stdout.flush()
         sys.exit()
-
-    if args.verbose is not None and args.quiet:
-        parser.error("Cannot specify both --verbose and --quiet!")
 
     if args.action is None:
         args.action = emit_json
@@ -232,9 +279,8 @@ async def consume_messages(action, queue: asyncio.Queue) -> None:
 async def loop():
     args = parse_cli_args()
 
-    if not args.quiet:
-        log_levels = [logging.ERROR, logging.CRITICAL, logging.WARNING, logging.INFO, logging.DEBUG]
-        logging.basicConfig(level=log_levels[min(args.verbose or 1, 4)])
+    if args.verbosity is not None:
+        logging.basicConfig(level=args.verbosity)
 
     msg_queue = asyncio.Queue()
 
@@ -242,6 +288,13 @@ async def loop():
         cons = [WISConsumer.from_uri(uri, include_topic=args.topic) for uri in args.uris]
     else:
         cons = [WISConsumer(include_topic=args.topic)]
+
+    if args.explain:
+        sys.stdout.write(f'Action: {args.action.__name__}\n')
+        sys.stdout.write("Connection(s):\n")
+        sys.stdout.write("\t\n".join(map(str, cons)))
+        sys.stdout.write("\n")
+        sys.exit(0)
 
     async with asyncio.TaskGroup() as tg:
         tg.create_task(consume_messages(args.action, msg_queue))
